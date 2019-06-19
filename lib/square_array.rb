859996f3d3959429399baac6582637b7c912e43f
Emily Lillie <emilylillie3@gmail.com>
2019-06-19 22:52:26 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,3 +3,17 @@ def square_array(array)
   new_array = array.each do n**2
   return new_array
 end
+
+#i think i tricked the system into thinking this would work
+#it definitely doesn't actually work
+
+#this is the right one 
+def square_array(array)
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
+end
